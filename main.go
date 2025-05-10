3c365ed6f62c4ac09246b80b4bcba0b20e55ff73
nsavinda <nirmalsavinda29@gmail.com>
2025-05-10 11:40:10 +0530

    Add initial project files with basic HTTP server implementation

---
+++ main.go
@@ -0,0 +1,26 @@
+package main
+
+import (
+	"fmt"
+	"net/http"
+	"os"
+)
+
+func helloHandler(w http.ResponseWriter, r *http.Request) {
+	fmt.Fprintln(w, "Hello, World!")
+}
+
+func main() {
+	port := os.Getenv("PORT")
+	if port == "" {
+		port = "8080" // fallback default port
+	}
+
+	http.HandleFunc("/", helloHandler)
+
+	addr := ":" + port
+	fmt.Printf("Server starting at http://localhost%s\n", addr)
+	if err := http.ListenAndServe(addr, nil); err != nil {
+		fmt.Println("Error starting server:", err)
+	}
+}
